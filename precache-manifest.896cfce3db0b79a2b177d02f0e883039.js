95a019eec78b34df8b28f97a28623140ba366b15
pankajmandal1996 <mandalpankaj10696@gmail.com>
2021-03-24 00:54:18 +0530

    Updates

---
+++ precache-manifest.896cfce3db0b79a2b177d02f0e883039.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21877498268b9cb245491b09dae9cfc4",
+    "url": "/getuserslist/index.html"
+  },
+  {
+    "revision": "40c76e2c2c31728d442d",
+    "url": "/getuserslist/static/css/2.9f3e9094.chunk.css"
+  },
+  {
+    "revision": "126452a34216a0beccb6",
+    "url": "/getuserslist/static/css/main.ec3e2189.chunk.css"
+  },
+  {
+    "revision": "40c76e2c2c31728d442d",
+    "url": "/getuserslist/static/js/2.5f102fa0.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/getuserslist/static/js/2.5f102fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126452a34216a0beccb6",
+    "url": "/getuserslist/static/js/main.387f08dd.chunk.js"
+  },
+  {
+    "revision": "1608bba66ab550f0a596",
+    "url": "/getuserslist/static/js/runtime-main.25c73b5e.js"
+  }
+]);
